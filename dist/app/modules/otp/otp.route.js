1857de76212253105459b126f253472bb9fa217a
Juwel Rana <juwel.jem@gmail.com>
2025-09-23 02:55:22 +0600

    Stop tracking dist folder

--- dist/app/modules/otp/otp.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OtpRoutes = void 0;
-const express_1 = require("express");
-const otp_controller_1 = require("./otp.controller");
-const route = (0, express_1.Router)();
-route.post("/send", otp_controller_1.OtpController.otpSend);
-route.post("/verify", otp_controller_1.OtpController.otpVerify);
-exports.OtpRoutes = route;
--- dist/app/modules/tour/tour.constraint.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchAbleField = void 0;
-exports.searchAbleField = ["title", "description", "location"];
